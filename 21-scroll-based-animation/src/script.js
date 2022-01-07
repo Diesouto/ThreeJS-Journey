f152516db6335b104732a58ee19c4f2858165406
Diesouto <diegosouto2000@gmail.com>
2022-01-07 11:16:22 +0100

    Video 21

--- 21-scroll-based-animation/src/script.js
+++ 21-scroll-based-animation/src/script.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import * as dat from 'lil-gui'
+import gsap from 'gsap'
 
 /**
  * Base
@@ -14,8 +15,22 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
+// Parameters
+const parameters = {
+    materialColor: '#ffeded',
+    objectDistance: 4
+}
+
+// Texture
+const textureLoader = new THREE.TextureLoader();
+const gradientTexture = textureLoader.load('/textures/gradients/3.jpg');
+gradientTexture.magFilter = THREE.NearestFilter;
+
 // Material
-const material = new THREE.MeshToonMaterial({ color: parameters.materialColor })
+const material = new THREE.MeshToonMaterial({ 
+    color: parameters.materialColor,
+    gradientMap: gradientTexture
+})
 
 // Meshes
 const mesh1 = new THREE.Mesh(
@@ -31,7 +46,44 @@ const mesh3 = new THREE.Mesh(
     material
 )
 
+const sectionMeshes = [ mesh1, mesh2, mesh3 ]
+
+mesh1.position.x = 2;
+mesh2.position.x = - 2;
+mesh3.position.x = 2;
+
+mesh1.position.y = - parameters.objectDistance * 0;
+mesh2.position.y = - parameters.objectDistance * 1;
+mesh3.position.y = - parameters.objectDistance * 2;
+
 scene.add(mesh1, mesh2, mesh3);
+
+/**
+ * Particles
+ */
+// Geometry
+const particlesCount = 200;
+const positions = new Float32Array(particlesCount * 3);
+
+for(let i = 0; i < particlesCount; i++) {
+    positions[i * 3 + 0] = (Math.random() - 0.5) * 10
+    positions[i * 3 + 1] = parameters.objectDistance * 0.5 - Math.random() * parameters.objectDistance * sectionMeshes.length
+    positions[i * 3 + 2] = (Math.random() - 0.5) * 10
+}
+
+const particlesGeometry = new THREE.BufferGeometry()
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+
+// Material
+const particlesMaterial = new THREE.PointsMaterial({
+    color: parameters.materialColor,
+    sizeAttenuation: true,
+    size: 0.03
+})
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
 
 /**
  * Lights
@@ -66,10 +118,14 @@ window.addEventListener('resize', () =>
 /**
  * Camera
  */
+// Group
+const cameraGroup = new THREE.Group() // We need a group to update multiple times the camera
+scene.add(cameraGroup)
+
 // Base camera
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 6
-scene.add(camera)
+cameraGroup.add(camera)
 
 /**
  * Renderer
@@ -83,13 +139,76 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.setClearAlpha(0)
 
 /**
+ * Scroll
+ */
+let scrollY = window.scrollY;
+let currentSection = 0;
+
+window.addEventListener('scroll', () => {
+    scrollY = window.scrollY
+    const newSection = Math.round(scrollY / sizes.height)
+
+    if(newSection != currentSection) {
+        currentSection = newSection;
+        // console.log('changed', currentSection)
+
+        gsap.to(
+            sectionMeshes[currentSection].rotation,
+            {
+                duration: 1.5,
+                ease: 'power2.inOut',
+                x: '+=6',
+                y: '+=3',
+                z: '+=1.5'
+            }
+        )
+    }
+
+    // console.log(scrollY);
+})
+
+/**
+ * Cursor
+ */
+let cursor = {};
+cursor.x = 0;
+cursor.y = 0;
+
+window.addEventListener('mousemove', (event) => {
+    cursor.x = (event.clientX / sizes.width) - 0.5;
+    cursor.y = (event.clientY / sizes.height) - 0.5;
+
+    // console.log(cursor);
+})
+
+/**
  * Animate
  */
 const clock = new THREE.Clock()
+let previousTime = 0;
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime; // Time spent between each frame (helps with experience on high frecuency screens)
+    previousTime = elapsedTime;
+
+    // console.log(deltaTime);
+
+    // Animate
+    for(const mesh of sectionMeshes) {
+        mesh.rotation.x += deltaTime * 0.1;
+        mesh.rotation.y += deltaTime * 0.12;
+    }
+
+    // Scroll
+    camera.position.y = - (scrollY / sizes.height) * parameters.objectDistance;
+
+    // Parallax
+    const parallaxX = cursor.x * 0.5;
+    const parallaxY = - cursor.y * 0.5;
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime; // add parallax effect with smoothing 
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime; 
 
     // Render
     renderer.render(scene, camera)
@@ -104,13 +223,10 @@ tick()
  * Debug
  */
  const gui = new dat.GUI()
-
- const parameters = {
-    materialColor: '#ffeded'
- }
  
  gui
     .addColor(parameters, 'materialColor')
     .onChange(() => {
-    material.color.set(parameters.materialColor);
+        material.color.set(parameters.materialColor);
+        particlesMaterial.color.set(parameters.materialColor);
     })
