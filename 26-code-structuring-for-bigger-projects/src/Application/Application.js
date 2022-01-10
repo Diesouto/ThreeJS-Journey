d723f7c931e00b948284556676f3725a99f0934e
Diesouto <diegosouto2000@gmail.com>
2022-01-10 15:57:30 +0100

    Video 26

---
+++ 26-code-structuring-for-bigger-projects/src/Application/Application.js
@@ -0,0 +1,50 @@
+import * as THREE from 'three';
+import Camera from "./Camera.js";
+import Renderer from './Renderer.js';
+import Sizes from "./Utils/Sizes.js";
+import Time from "./Utils/Time.js";
+
+let instance = null;
+
+export default class Application {
+    constructor(canvas) {
+        // Singleton
+        if(instance) {
+            return instance;
+        }
+        instance = this;
+
+        // Global access
+        window.application = this; 
+
+        // Options
+        this.canvas = canvas;
+
+        // Setup
+        this.sizes = new Sizes();
+        this.time = new Time();
+        this.scene = new THREE.Scene();
+        this.camera = new Camera();
+        this.renderer = new Renderer();
+
+        // Resize event
+        this.sizes.on('resize', () => {
+            this.resize();
+        })
+
+        // Time tick event
+        this.time.on('tick', () => {
+            this.update();
+        })
+    }
+
+    resize() {
+        this.camera.resize();
+        this.renderer.resize();
+    }
+
+    update(){
+        this.camera.update();
+        this.renderer.update();
+    }
+}
