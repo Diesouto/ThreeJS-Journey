0fba8b12b45e11818cd379adaff59743612e8cc6
Diesouto <diegosouto2000@gmail.com>
2022-01-11 18:34:44 +0100

    Video 27

---
+++ 27-shaders/src/shaders/test/rawFragment.glsl
@@ -0,0 +1,18 @@
+precision mediump float;
+
+uniform vec3 uColor;
+uniform sampler2D uTexture;
+
+varying float vRandom;
+varying vec2 vUv;
+varying float vElevation;
+
+void main() {
+    vec4 textureColor = texture2D(uTexture, vUv); // texture application
+    textureColor.rgb *= vElevation * 2.0 + 0.5; //shadow effect
+    gl_FragColor = textureColor;
+    // gl_FragColor = vec4(uColor, 1.0);
+    // gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0); // to change alpha -> transparent: true
+    // gl_FragColor = vec4(0.5, vRandom, 1.0, 1.0);
+}
+    
