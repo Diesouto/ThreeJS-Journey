23ee859f53b20d2ede32e1a5b568e4fd9f9fbe8f
Diesouto <diegosouto2000@gmail.com>
2022-01-14 16:14:15 +0100

    Video 29

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -1,4 +1,21 @@
+varying vec2 vUv;
+
 void main()
 {
-    gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
+    // float strength = vUv.x; // horizontal
+    // float strength = vUv.y; // vertical
+    // float strength = vUv.y - 1; // reversed vertical
+    // float strength = (vUv * 10, 1.0); // strong white
+
+    // float strength = mod(vUv.x * 10.0, 1.0); // wave effect
+    // strength = step(0.5, strength); // stronger wave effect
+    // strength += step(0.8, mod(vUv.y * 10.0, 1.0)); // makes a net with previous line
+
+    float strength = step(0.8, mod(vUv.x * 10.0, 1.0)); 
+    strength *= step(0.8, mod(vUv.y * 10.0, 1.0)); // Gets intesections between strength lines
+
+    // gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
+    // gl_FragColor = vec4(vUv, 1.0, 1.0);
+    // gl_FragColor = vec4(vUv, 0.0, 1.0);
+    gl_FragColor = vec4(vec3(strength), 1.0);
 }
